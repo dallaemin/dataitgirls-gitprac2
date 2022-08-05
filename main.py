da4532d2f982e127a059122d79c60ca495b8d308
joy <dallaemin.kim@gmail.com>
2022-08-05 16:04:08 +0900

    topics

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 '# hello world'
 " # hello"
+
+topics = [
+    {"id":1, "title":"commit", "view":100}, 
+    {"id":2, "title":"push", "view":200},
+    {"id":3, "title":"pull", "view":50},
+]
+topics
